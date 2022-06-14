7449ac7e52b17e86586b8bb26c73986101529181
atms <etiennedtboivin@gmail.com>
2022-06-14 15:13:48 -0700

    Suites 13, 14, 15

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -105,7 +105,7 @@ suite('Unit Tests', function () {
   suite('Strings', function () {
     // #13
     test('#isString, #isNotString', function () {
-      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
+      assert.isString(Math.sin(Math.PI / 4), 'A float is not a string');
       assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
       assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
