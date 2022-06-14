a924972fd813376781115231567c0fad286c628c
atms <etiennedtboivin@gmail.com>
2022-06-14 16:17:47 -0700

    famous italian explorers form suite

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -90,8 +90,8 @@ suite('Functional Tests with Zombie.js', function () {
       browser.fill('surname', 'Colombo').then(() => {
         browser.pressButton('submit', () => {
           browser.assert.success();
-          browser.assert.text('span#name', 'Marco');
-          browser.assert.text('span#surname', 'Polo');
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
           browser.assert.elements('span#dates', 1);
           done();
         })
